b5ade6eeee49a7ed2e82066abc98caf51a11b5d1
leapord <leapord@foxmail.com>
2022-09-12 13:58:25 +0800

    init

---
+++ src/views/login/locale/en-US.ts
@@ -0,0 +1,19 @@
+export default {
+  'login.form.title': 'Login to Promtheusx',
+  'login.form.userName.errMsg': 'Username cannot be empty',
+  'login.form.password.errMsg': 'Password cannot be empty',
+  'login.form.login.errMsg': 'Login error, refresh and try again',
+  'login.form.login.success': 'welcome to use',
+  'login.form.userName.placeholder': 'Username: admin',
+  'login.form.password.placeholder': 'Password: admin',
+  'login.form.rememberPassword': 'Remember password',
+  'login.form.forgetPassword': 'Forgot password',
+  'login.form.login': 'login',
+  'login.form.register': 'register account',
+  'login.banner.slogan1': 'a tool suite for prometheus',
+  'login.banner.subSlogan1':
+    'we make exporter config visible',
+  'login.banner.slogan2': 'all config base on http_sd',
+  'login.banner.subSlogan2':
+    'those can be config with web site no need to modify config file',
+};
